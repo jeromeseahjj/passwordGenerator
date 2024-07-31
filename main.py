54d3040a6a51fb06a9806ead3d0169236a06baac
jeromesjj <jerome.seahjj@gmail.com>
2024-07-31 22:38:20 +0800

    Added Error Exception

--- main.py
+++ main.py
@@ -3,23 +3,51 @@ from tkinter import *
 from tkinter import messagebox
 from password import generate
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def search():
+    website = entry_website.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=data[website], 
+                                message=f"Email: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 def save():
     website = entry_website.get()
     email = entry_email.get()
     password = entry_password.get()
+    new_data = {website: {
+        "email": email,
+        "password": password
+    }}
     
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
-                                    f"\nPassword: {password} \nIs it ok to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file) # Read the old data
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4) # Write everything to JSON
+        else:
+            data.update(new_data) # Update the existing dictionary
 
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4) # Write everything to JSON
+        finally:
             entry_website.delete(0, END)
             entry_password.delete(0, END)
 
@@ -48,7 +76,7 @@ label_password.grid(row=3, column=0)
 
 # Entries
 entry_website = Entry(width=35)
-entry_website.grid(row=1,column=1, columnspan=2)
+entry_website.grid(row=1,column=1)
 entry_website.focus()
 entry_email = Entry(width=35)
 entry_email.grid(row=2,column=1, columnspan=2)
@@ -59,6 +87,8 @@ entry_password.grid(row=3,column=1)
 # Button
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
+search_button = Button(text="Search", width=13, command=search)
+search_button.grid(row=1, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
